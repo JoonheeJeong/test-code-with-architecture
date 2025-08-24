f22a8e05981d8616b41b96de0e74f3f632747b89
Joonhee Jeong <jeonggoo75@gmail.com>
2025-08-24 19:13:36 +0900

    test: UserController#verifyEmail

--- src/test/java/com/example/demo/controller/UserControllerTest.java
+++ src/test/java/com/example/demo/controller/UserControllerTest.java
@@ -1,5 +1,6 @@
 package com.example.demo.controller;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -30,6 +31,9 @@ class UserControllerTest {
     @MockBean
     private JavaMailSender javaMailSender;
 
+    @Autowired
+    private ObjectMapper objectMapper;
+
     @DisplayName("GET /api/users/{id} 회원 조회시 200 UserResponse")
     @Test
     void getById_ok() throws Exception {
@@ -55,6 +59,48 @@ class UserControllerTest {
 
         // when, then
         mockMvc.perform(MockMvcRequestBuilders.get("/api/users/{id}", userId))
+                .andExpect(status().isNotFound())
+                .andExpect(content().string("Users에서 ID %d를 찾을 수 없습니다.".formatted(userId)))
+        ;
+    }
+
+    @DisplayName("GET /api/users/{id}/verify 이메일인증 성공시 Found 응답")
+    @Test
+    void verifyEmail_ok() throws Exception {
+        // given
+        long userId = 1L;
+        String certificationCode = "b84b2142-a620-4f95-b317-40f69c64fec8";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/{id}/verify", userId)
+                        .queryParam("certificationCode", certificationCode))
+                .andExpect(status().isFound())
+                .andExpect(header().string("Location", "http://localhost:3000"))
+        ;
+    }
+
+    @DisplayName("GET /api/users/{id}/verify 이메일인증 실패시 Forbidden 응답")
+    @Test
+    void verifyEmail_wrongVerification() throws Exception {
+        // given
+        long userId = 1L;
+        String certificationCode = "b84b2142-a620-4f95-b317-40f69c64fec9";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/{id}/verify", userId)
+                        .queryParam("certificationCode", certificationCode))
+                .andExpect(status().isForbidden())
+                .andExpect(content().string("자격 증명에 실패하였습니다."))
+        ;
+    }
+
+    @DisplayName("GET /api/users/{id}/verify 요청시 해당 id 회원 없으면 Not Found 응답")
+    @Test
+    void verifyEmail_nonexistentId_notFound() throws Exception {
+        // given
+        long userId = 3L;
+        String certificationCode = "b84b2142-a620-4f95-b317-40f69c64fec8";
+
+        mockMvc.perform(MockMvcRequestBuilders.get("/api/users/{id}/verify", userId)
+                        .queryParam("certificationCode", certificationCode))
                 .andExpect(status().isNotFound())
                 .andExpect(content().string("Users에서 ID %d를 찾을 수 없습니다.".formatted(userId)))
         ;
