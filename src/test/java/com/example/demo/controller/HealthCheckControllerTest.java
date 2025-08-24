bcaeef5c6f737b9a47f35f5a80f6819c58ea7e2c
Joonhee Jeong <jeonggoo75@gmail.com>
2025-08-24 19:31:46 +0900

    test-fix: HealthCheckController 테스트 JavaMailSender @MockBean 추가

--- src/test/java/com/example/demo/controller/HealthCheckControllerTest.java
+++ src/test/java/com/example/demo/controller/HealthCheckControllerTest.java
@@ -5,6 +5,8 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.mail.javamail.JavaMailSender;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 
@@ -17,6 +19,9 @@ class HealthCheckControllerTest {
 
     @Autowired
     private MockMvc mockMvc;
+
+    @MockBean
+    private JavaMailSender mailSender;
 
     @DisplayName("헬스 체크 ok")
     @Test
