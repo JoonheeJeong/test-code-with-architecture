cc901008aa592bf656593bc420ad2626ccd26d0c
Joonhee Jeong <jeonggoo75@gmail.com>
2025-08-24 18:54:54 +0900

    test: UserCreateController#create

---
+++ src/test/java/com/example/demo/controller/UserCreateControllerTest.java
@@ -0,0 +1,64 @@
+package com.example.demo.controller;
+
+import com.example.demo.model.dto.UserCreateDto;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import org.hamcrest.Matchers;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.MediaType;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.jdbc.Sql;
+import org.springframework.test.context.jdbc.SqlGroup;
+import org.springframework.test.web.servlet.MockMvc;
+import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
+import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
+
+import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.jsonPath;
+
+@SpringBootTest
+@AutoConfigureMockMvc
+@SqlGroup({
+        @Sql(value = "classpath:sql/test-user-controller-init.sql",
+                executionPhase = Sql.ExecutionPhase.BEFORE_TEST_METHOD),
+        @Sql(value = "classpath:sql/test-user-controller-end.sql",
+                executionPhase = Sql.ExecutionPhase.AFTER_TEST_METHOD)
+})
+class UserCreateControllerTest {
+
+    @Autowired
+    private MockMvc mockMvc;
+
+    @MockBean
+    private JavaMailSender mailSender;
+
+    @Autowired
+    private ObjectMapper objectMapper;
+
+    @DisplayName("POST /api/users 로 회원을 생성 성공시 201 및 UserResponse 응답")
+    @Test
+    void create_createdWithUserResponse() throws Exception {
+        // given
+        UserCreateDto dto = UserCreateDto.builder()
+                .email("test.email@example.com")
+                .nickname("test")
+                .address("Seoul")
+                .build();
+
+        // when, then
+        mockMvc.perform(MockMvcRequestBuilders.post("/api/users")
+                        .contentType(MediaType.APPLICATION_JSON)
+                        .content(objectMapper.writeValueAsString(dto)))
+                .andExpect(MockMvcResultMatchers.status().isCreated())
+                .andExpect(jsonPath("$.id").value(3))
+                .andExpect(jsonPath("$.email").value(dto.getEmail()))
+                .andExpect(jsonPath("$.nickname").value(dto.getNickname()))
+                .andExpect(jsonPath("$.status").value("PENDING"))
+                .andExpect(jsonPath("$.lastLoginAt").value(Matchers.nullValue()))
+        ;
+    }
+
+}
---
+++ src/test/resources/sql/test-user-controller-init.sql
@@ -0,0 +1,5 @@
+insert into users(`id`, `email`, `nickname`, `address`, `certification_code`, `status`, `last_login_at`)
+values
+    (1, 'jeonggoo75@gmail.com', 'jeonggoo75', 'Daejeon', 'b84b2142-a620-4f95-b317-40f69c64fec8', 'PENDING', 0),
+    (2, 'ownsider@naver.com', 'ownsider', 'Hanam', 'b84b2142-a620-4f95-b317-40f69c64fec9', 'ACTIVE', 1)
+    ;
